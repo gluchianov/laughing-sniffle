b504894b53898f6a00f853674333b232783f2dfe
Gheorghe Luchianov <gluchianov@live.com>
2023-05-22 13:13:56 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="logo512.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Codespaces <span className="love">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <p>
           <a
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            
           </a>
         </p>
       </header>
